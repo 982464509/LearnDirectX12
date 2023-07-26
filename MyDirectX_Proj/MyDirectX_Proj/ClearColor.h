564cd1d9a9ce9ee3e1e538d8cbf7a1d4e98577ab
zxz <982464509@qq.com>
2023-07-26 20:48:22 +0800

    [n]. 改名

---
+++ MyDirectX_Proj/MyDirectX_Proj/ClearColor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "BaseDX.h"
+
+using namespace DirectX;
+
+class ClearColor :public BaseDX
+{
+public:
+	ClearColor(uint32_t width, uint32_t height, std::wstring name);
+
+private:
+	
+	void OnInit() override;
+	void OnResize() override;
+	void OnLoadAssets() override;
+	void OnUpdate() override;
+	void OnRender() override;
+	void OnDestroy() override;
+};
